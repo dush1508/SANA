0096ab79893e5fed39d4a7d76d942339ad2a4b07
dushyanb <dushyanb@uci.edu>
2025-02-08 18:41:35 -0800

    Another Fix

--- src/measures/LPS_I.cpp
+++ src/measures/LPS_I.cpp
@@ -10,7 +10,7 @@ LPS_I::LPS_I(const Graph* G1, const Graph* G2)
 LPS_I::~LPS_I(){}
 
 double LPS_I::eval(const Alignment& A) {
-    double TruePositive = A.numAlignedEdges(*G1, *G2);
+    double TruePositive = A.computeNumAlignedEdges(*G1, *G2);
     double V1 = G1->getNumNodes();
     double omega = 1/2 * (V1) * (V1-1);
     double TrueNegative = omega - (G1->getNumEdges() + G2->numEdgesInNodeInducedSubgraph(A.asVector()) - TruePositive);
--- src/measures/LPS_L.cpp
+++ src/measures/LPS_L.cpp
@@ -11,7 +11,7 @@ LPS_L::~LPS_L(){}
 
 double LPS_L::eval(const Alignment& A) {
 
-    double TruePositive = A.numAlignedEdges(*G1, *G2);
+    double TruePositive = A.computeNumAlignedEdges(*G1, *G2);
     double V1 = G1->getNumNodes();
     double omega = 1/2 * (V1) * (V1-1);
     double TrueNegative = omega - (G1->getNumEdges() + G2->numEdgesInNodeInducedSubgraph(A.asVector()) - TruePositive);
